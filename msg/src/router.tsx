e204e18c34bcddd8d19392746ad35d3191e073e4
sjh <jihoseo2008@gmail.com>
2024-08-12 10:49:58 +0900

    회원가입 페이지

--- msg/src/router.tsx
+++ msg/src/router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function Router() {
   return (
@@ -8,6 +9,7 @@ function Router() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </BrowserRouter>
   );
