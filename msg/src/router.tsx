d7d8c0e4421aa09a70cacee7d253cd6e7d265b59
sjh <jihoseo2008@gmail.com>
2024-08-12 15:44:59 +0900

    설문조사 페이지 작성

--- msg/src/router.tsx
+++ msg/src/router.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainPage from "./pages/MainPage";
+import StartPage from "./pages/StartPage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
+import FormPage from "./pages/FormPage";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<StartPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/form" element={<FormPage />} />
       </Routes>
     </BrowserRouter>
   );
