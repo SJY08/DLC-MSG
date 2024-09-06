a9412cf5c49b1c2cdee5cf1640885916e665a5b4
sjh <jihoseo2008@gmail.com>
2024-09-06 15:59:26 +0900

    스크롤 설정

--- msg/src/router.tsx
+++ msg/src/router.tsx
@@ -12,25 +12,27 @@ import ClubPage from "./pages/Club/ClubPage"
 import NoticePost from "./pages/Club/NoticePost"
 import TopPost from "./pages/Club/TopPost"
 import AllPosts from "./pages/Club/AllPosts"
+import ScrollTop from "./pages/ScorllTop"
 
 function Router() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<StartPage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/signup" element={<SignUpPage />} />
-                <Route path="/form" element={<FormPage />} />
-                <Route path="/main" element={<MainPage />} />
-                <Route path="/mission" element={<MissionPage />} />
-                <Route path="/inform" element={<InformPage />} />
-                <Route path="/video" element={<AllVideo />} />
-                <Route path="/news" element={<AllNews />} />
-                <Route path="/club" element={<ClubPage />} />
-                <Route path="/notice" element={<NoticePost />} />
-                <Route path="/top" element={<TopPost />} />
-                <Route path="/posts" element={<AllPosts />} />
-            </Routes>
+            <ScrollTop />
+                <Routes>
+                    <Route path="/" element={<StartPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/signup" element={<SignUpPage />} />
+                    <Route path="/form" element={<FormPage />} />
+                    <Route path="/main" element={<MainPage />} />
+                    <Route path="/mission" element={<MissionPage />} />
+                    <Route path="/inform" element={<InformPage />} />
+                    <Route path="/video" element={<AllVideo />} />
+                    <Route path="/news" element={<AllNews />} />
+                    <Route path="/club" element={<ClubPage />} />
+                    <Route path="/notice" element={<NoticePost />} />
+                    <Route path="/top" element={<TopPost />} />
+                    <Route path="/posts" element={<AllPosts />} />
+                </Routes>
         </BrowserRouter>
     )
 }
